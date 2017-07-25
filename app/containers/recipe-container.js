0d8121962cc03d2f8098506037b64e18a834f747
Ciara Bujanos <c.buj22@gmail.com>
2017-07-24 21:27:59 -0600

    instructions rendering

--- app/containers/recipe-container.js
+++ app/containers/recipe-container.js
@@ -1,11 +1,15 @@
 import { connect } from 'react-redux'
 import Recipe from '../components/Recipe/Recipe'
+import { getInstructions } from '../actions';
+
 
 const mapStateToProps = (state) => {
   return {
     recipes: state.items
   }
 }
+
+
 
 
 export default connect(mapStateToProps, null)(Recipe)
